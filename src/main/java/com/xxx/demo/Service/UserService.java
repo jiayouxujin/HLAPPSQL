63c9c9fba1575ca02ae5a39127e4d1c772ca1ca5
HicoderDR <36002055+HicoderDR@users.noreply.github.com>
2019-04-19 09:10:13 +0800

    map的进一步完成

--- src/main/java/com/xxx/demo/Service/UserService.java
+++ src/main/java/com/xxx/demo/Service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service("userService")
 public class UserService {
@@ -19,6 +20,10 @@ public class UserService {
         } catch (Exception e) {
             return null;
         }
+    }
+    public List<User> getuserlist(){
+        List<User> list=userRepository.getuserlist();
+        return list;
     }
 
     public User searchUserByName(String username) {
